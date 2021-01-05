b59caec1fec90603d9c81c4302779291efc7028d
JiaZheng <735577921@qq.com>
2021-01-05 21:30:00 +0800

    binary search tree

--- data-structure/src/main/java/com/jeng/datastructure/tree/BinarySearchTree.java
+++ data-structure/src/main/java/com/jeng/datastructure/tree/BinarySearchTree.java
@@ -7,14 +7,14 @@ import java.util.Stack;
 
 public class BinarySearchTree<T> {
 
-    private Node<T> root;
+    private TreeNode<T> root;
 
     private Comparator<T> comparator;
 
     private int size;
 
     public BinarySearchTree(T data, Comparator<T> comparator) {
-        this.root = new Node<>(data);
+        this.root = new TreeNode<>(data);
         this.comparator = comparator;
         size++;
     }
@@ -28,26 +28,26 @@ public class BinarySearchTree<T> {
      * @param value
      */
     public void insert(T value) {
-        Node<T> newNode = new Node<>(value);
+        TreeNode<T> newTreeNode = new TreeNode<>(value);
         if (null == root) {
-            root = newNode;
+            root = newTreeNode;
             return;
         }
 
-        Node<T> cur = root;
+        TreeNode<T> cur = root;
         while (null != cur) {
             if (comparator.compare(value, cur.data) > 0) {
                 if (cur.right == null) {
-                    newNode.parent = cur;
-                    cur.right = newNode;
+                    newTreeNode.parent = cur;
+                    cur.right = newTreeNode;
                     size++;
                     return;
                 }
                 cur = cur.right;
             } else {
                 if (cur.left == null) {
-                    newNode.parent = cur;
-                    cur.left = newNode;
+                    newTreeNode.parent = cur;
+                    cur.left = newTreeNode;
                     size++;
                     return;
                 }
@@ -62,10 +62,10 @@ public class BinarySearchTree<T> {
      * @return
      */
     public List<T> inorderTraversal() {
-        Stack<Node<T>> stack = new Stack<>();
+        Stack<TreeNode<T>> stack = new Stack<>();
         List<T> result = new ArrayList<>();
 
-        Node<T> cur = root;
+        TreeNode<T> cur = root;
         while (cur !=null || !stack.isEmpty()) {
             while (cur != null) {
                 stack.push(cur);
@@ -84,8 +84,8 @@ public class BinarySearchTree<T> {
      * 按值查找
      *
      * */
-    public  Node<T> find(T val){
-        Node<T> cur = root;
+    public TreeNode<T> find(T val){
+        TreeNode<T> cur = root;
         while (null != cur) {
             if (cur.data.equals(val)) {
                 return cur;
@@ -99,7 +99,7 @@ public class BinarySearchTree<T> {
     }
 
     public T findMin(){
-        Node<T> cur = root;
+        TreeNode<T> cur = root;
         while (null != cur.left) {
             cur = cur.left;
         }
@@ -107,7 +107,7 @@ public class BinarySearchTree<T> {
     }
 
     public T findMax() {
-        Node<T> cur = root;
+        TreeNode<T> cur = root;
         while (null != cur.right) {
             cur = cur.right;
         }
@@ -117,10 +117,12 @@ public class BinarySearchTree<T> {
     /**
      * 1.要删除的t节点下没有其他子节点（即要删除的是叶子节点）
      * 2.要删除的t节点下有一个子节点——将t的父节点和子节点对接（左or右）
-     * 3.要删除的t节点下由两个子节点——将t的父节点和右节点对接（左or右），将t的左子树对接到右子树最小节点的左子节点
+     * 3.要删除的t节点下由两个子节点
+     *  ——1.将t的父节点和右节点对接（左or右），将t的左子树对接到右子树最小节点的左子节点
+     *  ——2.找到t的中序遍历的后续节点k，用k替换t
      * */
-    public boolean delete(T val) {
-        Node<T> cur = root;
+    public boolean delete1(T val) {
+        TreeNode<T> cur = root;
         // 查找节点 —— Node<T> tNode = find(val);
         while (cur != null) {
             if (cur.data.equals(val)) {
@@ -136,7 +138,7 @@ public class BinarySearchTree<T> {
             return false;
         }
 
-        Node<T> p = cur.parent;
+        TreeNode<T> p = cur.parent;
         // 要删除的叶子节点
         if (cur.left == null && cur.right == null) {
             if (null == p) {// 跟节点
@@ -147,12 +149,12 @@ public class BinarySearchTree<T> {
                 p.right = null;
             }
         } else if (cur.left != null && cur.right != null) {
-            Node<T> minNode = cur.right;
-            while (minNode.left != null) {
-                minNode = minNode.left;
+            TreeNode<T> minTreeNode = cur.right;
+            while (minTreeNode.left != null) {
+                minTreeNode = minTreeNode.left;
             }
-            minNode.left = cur.left;
-            cur.left.parent = minNode;
+            minTreeNode.left = cur.left;
+            cur.left.parent = minTreeNode;
 
             if (p == null) {// 根节点
                 root = cur.right;
@@ -188,6 +190,78 @@ public class BinarySearchTree<T> {
     }
 
     /**
+     * 1.要删除的t节点下没有其他子节点（即要删除的是叶子节点）
+     * 2.要删除的t节点下有一个子节点——将t的父节点和子节点对接（左or右）
+     * 3.要删除的t节点下由两个子节点
+     *  ——1.将t的父节点和右节点对接（左or右），将t的左子树对接到右子树最小节点的左子节点
+     *  ——2.找到t的中序遍历的后续节点k，用k替换t
+     * */
+    public boolean delete2(T data){
+        TreeNode<T> target = find(data);
+        if (target == null) {
+            return false;
+        }
+
+        // 叶子节点
+        if (target.left == null && target.right == null) {
+            if (root == target) { // 根节点
+                root = null;
+            } else {
+                setNewChild(target.parent, target, null);
+            }
+            return true;
+        }
+
+        // 非叶子节点，找到t的中序遍历的后续节点k(右子树的最小节点)，用k替换t
+        if (target.right != null && target.left != null) {
+            TreeNode<T> rCur = target.right;
+            while (rCur.left != null) {
+                rCur = rCur.left;
+            }
+
+            target.data = rCur.data;
+            target = rCur;
+        }
+
+        // 找到新的子节点
+        TreeNode<T> child;
+        if (target.left != null) {
+            child = target.left;
+        } else if (target.right != null) {
+            child = target.right;
+        } else {
+            child = null;
+        }
+
+        // 找父节点
+        if (target.parent == null) { // 跟节点
+            root = child;
+            root.parent = null;
+        } else if (target.parent.left == target) {
+            target.parent.left = child;
+        } else {
+            target.parent.right = child;
+        }
+        return true;
+    }
+
+
+    private void setNewChild(TreeNode<T> parent, TreeNode<T> oldChild, TreeNode<T> newChild) {
+        if (parent.left == oldChild) {
+            parent.left = newChild;
+        } else if (parent.right == oldChild) {
+            parent.right = newChild;
+        } else {
+            throw new RuntimeException("parent不是child的父节点");
+        }
+
+        if (null != newChild){
+            newChild.parent = parent;
+        }
+    }
+
+
+    /**
      * 使用递归进行中序遍历
      *
      * @return
@@ -198,7 +272,7 @@ public class BinarySearchTree<T> {
         return result;
     }
 
-    private void inorderRecursion(Node<T> root, List<T> result) {
+    private void inorderRecursion(TreeNode<T> root, List<T> result) {
         if (null == root) {
             return;
         }
@@ -211,7 +285,7 @@ public class BinarySearchTree<T> {
         this.comparator = comparator;
     }
 
-    public Node<T> getRoot() {
+    public TreeNode<T> getRoot() {
         return root;
     }
 
@@ -219,13 +293,13 @@ public class BinarySearchTree<T> {
         return size;
     }
 
-    private static class Node<T> {
+    private static class TreeNode<T> {
         private T data;
-        private Node<T> parent;
-        private Node<T> left;
-        private Node<T> right;
+        private TreeNode<T> parent;
+        private TreeNode<T> left;
+        private TreeNode<T> right;
 
-        Node(T data) {
+        TreeNode(T data) {
             this.data = data;
         }
     }
@@ -251,16 +325,17 @@ public class BinarySearchTree<T> {
         List<Integer> result1 = bst.inorderTraversal();
         System.out.println(result1);
 
-        Node<Integer> targetNode =  bst.find(4);
-        System.out.println(targetNode.data);
+        TreeNode<Integer> targetTreeNode =  bst.find(3);
+        System.out.println(targetTreeNode.data);
 
         System.out.println("min=" + bst.findMin());
         System.out.println("max=" + bst.findMax());
-        System.out.println(bst.root.data);
+        System.out.println("root="+bst.root.data);
 
-        bst.delete(2);
+        bst.delete2(3);
         List<Integer> result2 = bst.inorderTraversal();
         System.out.println(result2);
+        System.out.println("root="+bst.root.data);
 
     }
 }
