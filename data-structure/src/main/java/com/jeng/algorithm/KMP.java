7952a955ec065789d2e42efa66093c65d7e1122f
JiaZheng <735577921@qq.com>
2021-01-12 19:46:52 +0800

    kmp

--- data-structure/src/main/java/com/jeng/algorithm/KMP.java
+++ data-structure/src/main/java/com/jeng/algorithm/KMP.java
@@ -3,14 +3,14 @@ package com.jeng.algorithm;
 public class KMP {
 
     public static int[] getNextArray(char[] t) {
-        int[] next = new int[t.length];
+        int[] next = new int[t.length+1];
         next[0] = -1;
         next[1] = 0;
         int k;
-        for (int j = 2; j < t.length; j++) {
-            k = next[j - 1];// 数据当前最后一个元素
+        for (int j = 2; j <= t.length; j++) {
+            k = next[j - 1];// 前一个元素的最大公共前缀
             while (k != -1) {
-                if (t[j - 1] == t[k]) { // 字符串当前元素于第k元素相同，下一个字符的下标为k+1
+                if (t[j - 1] == t[k]) { //
                     next[j] = k + 1;
                     break;
                 } else {
@@ -22,17 +22,47 @@ public class KMP {
         return next;
     }
 
+    public static int[] getNextArray2(char[] t) {
+        int[] next = new int[t.length+1];
+        next[0] = -1;
+        next[1] = 0;
+        int k;
+        for (int j = 2; j <= t.length; j++) {
+            k = next[j - 1];// 前一个元素的最大公共前缀字符个数
+            while (k != -1) {
+                if (t[j-1] == t[k]) { //
+                    next[j] = k + 1;
+                    break;
+                } else {
+                    k = next[k];    // 从前一个最大公共的前缀找出其最大前后缀（套娃）
+                }
+            }
+
+            if (-1 == k) {
+                next[j] = 0;
+            }
+        }
+        return next;
+    }
+
 
 
     public static void main(String[] args) {
-        char[] chars = "acaacab".toCharArray();
+        char[] chars = "acacadacacacd".toCharArray();
         int[] nextArray = getNextArray(chars);
+        int[] nextArray2 = getNextArray2(chars);
 
+        System.out.print("   ");
         for (char aChar : chars) {
             System.out.print(aChar+",");
         }
         System.out.println();
         for (int i : nextArray) {
+            System.out.print(i+",");
+        }
+
+        System.out.println();
+        for (int i : nextArray2) {
             System.out.print(i+",");
         }
     }
---
+++ leetcode/src/main/java/com/jeng/leetcode/string/L_1392.java
@@ -0,0 +1,37 @@
+package com.jeng.leetcode.string;
+
+/**
+ * 1392.最长快乐前缀
+ *
+ */
+public class L_1392 {
+    public String longestPrefix(String s) {
+        if (null == s || s.length() == 0) {
+            return "";
+        }
+        int[] next = new int[s.length()+1];
+        next[0] = -1;
+        next[1] = 0;
+        int k;
+        for (int i = 2; i <= s.length(); i++) {
+            k = next[i - 1];
+            while (k != -1) {
+                if (s.charAt(i - 1) == s.charAt(k)) {
+                    next[i] = k + 1;
+                    break;
+                } else {
+                    k = next[k];
+                }
+            }
+            if (-1 == k) {
+                next[i] = 0;
+            }
+        }
+
+        if (next[next.length - 1] > 0) {
+            return s.substring(0, next[next.length - 1]);
+        } else {
+            return "";
+        }
+    }
+}
