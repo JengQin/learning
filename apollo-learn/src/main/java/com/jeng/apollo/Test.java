d55f5e1230873ceefb41d2d6821255832e8b5820
JiaZheng <735577921@qq.com>
2021-06-08 19:22:04 +0800

    add apollo

--- apollo-learn/src/main/java/com/jeng/apollo/Test.java
+++ apollo-learn/src/main/java/com/jeng/apollo/Test.java
@@ -1,7 +1,10 @@
 package com.jeng.apollo;
 
 import com.ctrip.framework.apollo.Config;
+import com.ctrip.framework.apollo.ConfigChangeListener;
 import com.ctrip.framework.apollo.ConfigService;
+import com.ctrip.framework.apollo.model.ConfigChange;
+import com.ctrip.framework.apollo.model.ConfigChangeEvent;
 
 /**
  * The Test
@@ -12,12 +15,27 @@ import com.ctrip.framework.apollo.ConfigService;
 public class Test {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("env", "DEV");
         Config config = ConfigService.getAppConfig();
         String key = "dsp.application.name";
         String defaultValue = "000";
         String value = config.getProperty(key, defaultValue);
 
         System.out.println(value);
+
+
+        config.addChangeListener(new ConfigChangeListener() {
+            @Override
+            public void onChange(ConfigChangeEvent changeEvent) {
+                System.out.println("changes for namespace" + changeEvent.getNamespace());
+                for (String key : changeEvent.changedKeys()) {
+                    ConfigChange change = changeEvent.getChange(key);
+                    System.out.println(String.format("%s, %s: %s ---> %s", change.getChangeType(), change.getPropertyName(), change.getOldValue(), change.getNewValue()));
+                }
+            }
+        });
+
+        Thread.sleep(100000);
     }
 }
